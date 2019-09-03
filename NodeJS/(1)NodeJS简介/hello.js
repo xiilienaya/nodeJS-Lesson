1ed1cbda4507371ac70ca7b343c26107eb18c20f
安文悦 <33642932+xiilienaya@users.noreply.github.com>
2019-09-03 15:03:37 +0800

    Delete hello.js

--- NodeJS/(1)NodeJS简介/hello.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-    
-var server = http.createServer(function(req,res){
-    res.write("hello shinan");
-    res.end();
-});
-
-server.listen(8081);
-console.log("shinandashabi");
